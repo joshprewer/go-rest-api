605ceb4fcb1c616fa0994d464d5979514ef3c294
Josh Prewer <joshua.prewer@imaginecurve.com>
2022-05-15 17:27:34 +0100

    add db

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"internal/db/db"
+)
 
 // Setup app layers
 func Run() error {
 	fmt.Println("starting app")
+
+	db, err = db.NewDatabase()
+
+	if err != nil {
+		fmt.Println("Failed to connect to database")
+		return err
+	}
+
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/TutorialEdge/go-rest-api-course
 
 go 1.18
+
+require (
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/lib/pq v1.10.5 // indirect
+)
