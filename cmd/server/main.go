faed785c928b46bfba6ece024a5e73ff70c058e6
Josh Prewer <joshua.prewer@imaginecurve.com>
2022-05-16 09:36:01 +0100

    Add db service

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,23 +4,31 @@ import (
 	"context"
 	"fmt"
 
-	"internal/db/db"
+	"github.com/joshprewer/go-rest-api/internal/comment"
+	"github.com/joshprewer/go-rest-api/internal/db"
 )
 
 // Setup app layers
 func Run() error {
 	fmt.Println("starting app")
 
-	db, err = db.NewDatabase()
+	db, err := db.NewDatabase()
 
 	if err != nil {
-		fmt.Println("Failed to connect to database")
+		fmt.Println("failed to connect to databse")
 		return err
 	}
 
-	if err := db.Ping(context.Background()); err != nil {
+	if err := db.MigrateDB(); err != nil {
+		fmt.Println("failed to migrate db")
 		return err
 	}
+
+	cmtService := comment.NewService(db)
+	fmt.Println(cmtService.GetComment(
+		context.Background(),
+		"b2059fe8-f11d-41e7-9b12-2250642ef27d",
+	))
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,12 @@
-module github.com/TutorialEdge/go-rest-api-course
+module github.com/joshprewer/go-rest-api
 
 go 1.18
 
 require (
+	github.com/golang-migrate/migrate/v4 v4.15.2 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jmoiron/sqlx v1.3.5 // indirect
 	github.com/lib/pq v1.10.5 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
 )
